e3e58c6ce77827e92f65ff48c167cfea88ec8bcd
Mariia Derkach <mary.moss333@gmail.com>
2020-10-02 18:24:49 +0300

    fix: solve array problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array===undefined){
+    return 0
+  }
+  else if(array.length===0){
+    return 0
+  }
+  else{
+    let b = array.sort((a , b)=>{
+      return(a-b)
+    })
+    return b[0];
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array===undefined){
+    return 0
+  }
+  else if(array.length===0){
+    return 0
+  }
+  else{
+    let b = array.sort((a , b)=>(a-b));
+    return b[b.length-1];
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array===undefined){
+    return 0
+  }
+  else if(array.length===0){
+    return 0
+  }
+  else{
+    let b = array.reduce((accumulator, currentValue) => {
+      return accumulator + currentValue;
+    }, 0);
+    let c = b/(array.length);
+    c.toFixed(2)
+    return c;
+  }
 }
